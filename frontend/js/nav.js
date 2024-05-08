84563e5007a7f19c0d58e5200b1c7e1192b01d2f
Mohamed <mohamedabuassaf@gmail.com>
2024-05-08 21:50:04 +0300

    added the navigation to all the pages in the website

---
+++ frontend/js/nav.js
@@ -0,0 +1,15 @@
+const header = document.querySelector('header')
+
+header.innerHTML = `
+<nav class="navigation">
+<img src="../images/logo1.png" alt="" class="logo">
+
+<ul>
+    <li><a href="/frontend/html/index.html">Home</a></li>
+    <li><a href="/frontend/html/products.html">Products</a></li>
+    <li><a href="#">Contact</a></li>
+</ul>
+
+<a href="/frontend/html/login.html"><button type="button" class="login">Login</button></a>
+</nav>
+`
