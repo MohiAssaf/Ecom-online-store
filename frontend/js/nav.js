dfa9a748b42280a40a0d9d43f01307027a63bc8d
Mohamed <mohamedabuassaf@gmail.com>
2024-05-10 21:38:27 +0300

    added the contact page

--- frontend/js/nav.js
+++ frontend/js/nav.js
@@ -7,7 +7,7 @@ header.innerHTML = `
 <ul>
     <li><a href="/frontend/html/index.html">Home</a></li>
     <li><a href="/frontend/html/products.html">Products</a></li>
-    <li><a href="#">Contact</a></li>
+    <li><a href="/frontend/html/contact.html">Contact</a></li>
 </ul>
 
 <a href="/frontend/html/login.html"><button type="button" class="login">Login</button></a>
