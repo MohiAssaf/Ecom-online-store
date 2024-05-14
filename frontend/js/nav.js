d4a55e193a4f4d2525dead53574db41596160f67
Mohamed <mohamedabuassaf@gmail.com>
2024-05-14 23:09:23 +0300

    added the shoping cart icon to the navigation

--- frontend/js/nav.js
+++ frontend/js/nav.js
@@ -8,6 +8,7 @@ header.innerHTML = `
     <li><a href="/frontend/html/index.html">Home</a></li>
     <li><a href="/frontend/html/products.html">Products</a></li>
     <li><a href="/frontend/html/contact.html">Contact</a></li>
+    <li><i class="fa-solid fa-bag-shopping"></i></li>
 </ul>
 
 <a href="/frontend/html/login.html"><button type="button" class="login">Login</button></a>
