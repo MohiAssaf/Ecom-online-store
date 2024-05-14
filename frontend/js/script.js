7f9665ec3a53b2444a0bad8b3ed26de48d337c1b
Mohamed <mohamedabuassaf@gmail.com>
2024-05-14 23:27:11 +0300

    added the event lisitner for shopping cart

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,16 +1,25 @@
-const header = document.querySelector('header')
+document.addEventListener('DOMContentLoaded', () => {
+    const header = document.querySelector('header');
 
-header.innerHTML = `
-<nav class="navigation">
-<img src="../images/logo1.png" alt="" class="logo">
+    header.innerHTML = `
+    <nav class="navigation">
+        <img src="../images/logo1.png" alt="" class="logo">
+    
+        <ul>
+            <li><a href="/frontend/html/index.html">Home</a></li>
+            <li><a href="/frontend/html/products.html">Products</a></li>
+            <li><a href="/frontend/html/contact.html">Contact</a></li>
+            <li class="shopping-cart"><i class="fa-solid fa-bag-shopping"></i></li>
+        </ul>
+    
+        <a href="/frontend/html/login.html"><button type="button" class="login">Login</button></a>
+    </nav>
+    `;
 
-<ul>
-    <li><a href="/frontend/html/index.html">Home</a></li>
-    <li><a href="/frontend/html/products.html">Products</a></li>
-    <li><a href="/frontend/html/contact.html">Contact</a></li>
-    <li><i class="fa-solid fa-bag-shopping"></i></li>
-</ul>
+    const shoppingCart = header.querySelector('.shopping-cart');
+    console.log(shoppingCart);
 
-<a href="/frontend/html/login.html"><button type="button" class="login">Login</button></a>
-</nav>
-`
+    shoppingCart.addEventListener('click', () => {
+        console.log('Shopping Cart clicked');
+    });
+});
