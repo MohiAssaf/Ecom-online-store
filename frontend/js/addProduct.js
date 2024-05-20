211e11d733678a1021716a18c3e5bcaa17fb2153
Mohamed <mohamedabuassaf@gmail.com>
2024-05-20 22:23:57 +0300

    added events for all the products in the products page

--- frontend/js/addProduct.js
+++ frontend/js/addProduct.js
@@ -1,9 +1,14 @@
 let addProductBtns = document.querySelectorAll('.product-addtocart');
 
 
-addProductBtns.forEach((productBtn) => {
-    productBtn.addEventListener('click', (e) => {
-        console.log(`Product Item ${e.target} was added`)
+document.addEventListener('DOMContentLoaded', () => {
+    let cart = document.querySelector('.cart-orders');
+
+    addProductBtns.forEach((productBtn) => {
+        productBtn.addEventListener('click', (e) => {
+            let pBtnParent = e.target.parentElement.parentElement
+            console.log(`Product Item ${e.target} was added`)
+            console.log(pBtnParent.img)
+        })
     })
 })
-
