87e5addc9322c8cbdbbf5733c63939552c2be6cd
Mohamed <mohamedabuassaf@gmail.com>
2024-05-20 22:35:49 +0300

    add to cart button works in the products page

--- frontend/js/addProduct.js
+++ frontend/js/addProduct.js
@@ -6,9 +6,26 @@ document.addEventListener('DOMContentLoaded', () => {
 
     addProductBtns.forEach((productBtn) => {
         productBtn.addEventListener('click', (e) => {
-            let pBtnParent = e.target.parentElement.parentElement
-            console.log(`Product Item ${e.target} was added`)
-            console.log(pBtnParent.img)
+            let productCard = e.target.parentElement.parentElement
+            let productImage = productCard.querySelector('.product-img').src;
+            let productName = productCard.querySelector('.product-name').innerText;
+            let productPrice = productCard.querySelector('.product-price').innerText;
+
+            cart.innerHTML += `
+            <div class="cart-orders">
+            
+                <div class="cart-box">
+                    <img src="${productImage}" alt="" class="cart-img">
+                    <div class="cart-details">
+                        <div class="cart-title">${productName}</div>
+                        <div class="cart-price">${productPrice}</div>
+                        <input type="number" value="1" class="cart-quantity">
+                    </div>
+
+                    <i id="remove-item" class="fa-solid fa-trash"></i> 
+                </div>
+
+            </div>`
         })
     })
 })
