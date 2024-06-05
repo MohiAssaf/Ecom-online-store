2301c9fe71597d389b14dbf84af575ec8d854fff
Mohamed <mohamedabuassaf@gmail.com>
2024-06-05 22:59:58 +0300

    table creation for users

--- backend/db.py
+++ backend/db.py
@@ -4,3 +4,23 @@ from config import db_config
 def connect_db():
     connection = psycopg2.connect(**db_config)
     return connection
+
+def create_user_table():
+    conn = connect_db()
+    cursor = conn.cursor()
+    
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        first_name VARCHAR(50) NOT NULL,
+        last_name VARCHAR(50) NOT NULL,
+        email VARCHAR(50) NOT NULL,
+        username VARCHAR(50) NOT NULL,
+        password VARCHAR(250) NOT NULL
+    )             
+''')
+    
+    conn.commit()
+    cursor.close()
+    conn.close()
+    
