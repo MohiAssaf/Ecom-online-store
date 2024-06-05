ab9ed50dbf99e0382ac1742cc03509d42d35977e
Mohamed <mohamedabuassaf@gmail.com>
2024-06-05 23:10:41 +0300

    func to generate captch img with generated captch text

--- backend/captch_generator.py
+++ backend/captch_generator.py
@@ -1,6 +1,18 @@
 import string
 import random
+from PIL import Image, ImageDraw, ImageFont
+
 
 def generate_captcha_text():
     return ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
 
+
+
+def generate_captcha_img(text):
+    font = ImageFont.load_default(10)
+    image = Image.new('RGB', (70, 25), color=(196, 164, 132))
+    draw = ImageDraw.Draw(image)
+    draw.text((10, 5), text, font=font, fill=(0, 0, 0))
+    return image
+    
+    
