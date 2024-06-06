c8d38d711257cefe426168b2ca562145fb338868
Mohamed <mohamedabuassaf@gmail.com>
2024-06-06 21:41:37 +0300

    added get handling for the register page in the http server

--- backend/server.py
+++ backend/server.py
@@ -1,11 +1,35 @@
 import socketserver
 import io
+import base64
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import urlparse, parse_qs
 from db import create_user_table
+from captch_generator import generate_captcha_text, generate_captcha_img
 
 
 class RequestHandler(BaseHTTPRequestHandler):
+    
+    def do_GET(self):
+        parsed_path = urlparse(self.path)
+        path = parsed_path.path
+        
+        if path == '/register':
+            captcha_txt = generate_captcha_text()
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.send_header('Set-Cookie', f'captcha={captcha_txt}; Path=/')
+            self.end_headers()
+            
+            with open('../frontend/html/register.html', 'r') as file:
+                html = file.read()
+                captcha_img = generate_captcha_img(captcha_txt)
+                with io.BytesIO() as output:
+                    captcha_img.save(output, format='PNG')
+                    captcha_data = output.getvalue()
+                captch_bs64 = base64.b64encode(captcha_data).decode('utf-8')
+                html = html.replace('{captcha_img}', f'<img src="data:image/png;base64,{captch_bs64}" alt="CAPTCHA">')
+                self.wfile.write(html.encode('utf-8'))
+                    
     
     def do_POST(self):
         parsed_path = urlparse(self.path)
