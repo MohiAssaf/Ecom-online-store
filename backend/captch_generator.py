ba5495a756169c415bc53b08a2a44e577385167b
Mohamed <mohamedabuassaf@gmail.com>
2024-06-08 15:39:23 +0300

    updated the styling and position of the catch container

--- backend/captch_generator.py
+++ backend/captch_generator.py
@@ -9,8 +9,8 @@ def generate_captcha_text():
 
 
 def generate_captcha_img(text):
-    font = ImageFont.load_default(10)
-    image = Image.new('RGB', (70, 25), color=(196, 164, 132))
+    font = ImageFont.load_default(15)
+    image = Image.new('RGB', (80, 30), color=(196, 164, 132))
     draw = ImageDraw.Draw(image)
     draw.text((10, 5), text, font=font, fill=(0, 0, 0))
     return image
