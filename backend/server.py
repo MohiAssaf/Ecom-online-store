07bae25b6df589fbb6f30e17347499bad22d0757
Mohamed <mohamedabuassaf@gmail.com>
2024-06-09 18:34:34 +0300

    adding the 404 case when a file is not found

--- backend/server.py
+++ backend/server.py
@@ -95,6 +95,12 @@ class RequestHandler(BaseHTTPRequestHandler):
                     with open(static_path, 'rb') as file:
                         self.wfile.write(file.read())
                         
+                else:
+                    self.send_response(404)
+                    self.send_header('Content-type', 'text/html')
+                    self.end_headers()
+                    self.wfile.write(b'404 File not found')
+                        
                 
         except Exception as ex:
             
