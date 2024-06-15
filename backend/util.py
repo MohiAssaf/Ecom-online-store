ebb6368a4ada71e36b9b713768f49df87ffc2f33
Mohamed <mohamedabuassaf@gmail.com>
2024-06-15 14:05:33 +0300

    func to generate a random session id

---
+++ backend/util.py
@@ -0,0 +1,23 @@
+import string
+import random
+import hashlib
+import secrets
+from PIL import Image, ImageDraw, ImageFont
+
+
+def generate_captcha_text():
+    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
+
+
+
+def generate_captcha_img(text):
+    font = ImageFont.load_default(15)
+    image = Image.new('RGB', (80, 30), color=(196, 164, 132))
+    draw = ImageDraw.Draw(image)
+    draw.text((10, 5), text, font=font, fill=(0, 0, 0))
+    return image
+    
+    
+def generate_session_id():
+    return hashlib.sha256(secrets.token_bytes(32)).hexdigest()
+    
